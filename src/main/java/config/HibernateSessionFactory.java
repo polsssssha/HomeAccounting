d2a4b3b0840228ab8d653b9cd3a7dc169adea625
polsssha <Nusty11102003@gmail.com>
2023-11-23 15:59:38 +0500

    1.2

---
+++ src/main/java/config/HibernateSessionFactory.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactory() {}
+
+    public synchronized static SessionFactory getSessionFactory() {
+        if (sessionFactory == null){
+            try {
+                final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                        .configure().build();
+                sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
